06fdd1e9c1a749f2e82c747fb27ab1caafc6db4f
pandega31 <pabditama1411@gmail.com>
2026-09-17 22:30:55 +0700

    [P-4] tambah master layout blade dan view index semua resource

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -8,8 +8,9 @@ use Illuminate\Http\Request;
 class MemberController extends Controller
 {
     private array $members = [
-        ['id' => 1, 'nama' => 'Pandega Surya', 'nim' => '3125600091', 'email' => 'pandega@example.com', 'nomor_telepon' => '081234567890', 'alamat' => 'Surabaya', 'status' => 'aktif'],
-        ['id' => 2, 'nama' => 'Abditama', 'nim' => '312560002', 'email' => 'surya@example.com', 'nomor_telepon' => '089876543210', 'alamat' => 'Sidoarjo', 'status' => 'aktif'],
+        ['id' => 1, 'nama' => 'Siti Aminah', 'nim' => '2310501001', 'email' => 'siti.aminah@pens.ac.id', 'nomor_telepon' => '081234567890', 'status' => 'aktif'],
+        ['id' => 2, 'nama' => 'Budi Santoso', 'nim' => '2310501002', 'email' => 'budi.santoso@pens.ac.id', 'nomor_telepon' => '081298765432', 'status' => 'aktif'],
+        ['id' => 3, 'nama' => 'Dewi Lestari', 'nim' => '2310501003', 'email' => 'dewi.lestari@pens.ac.id', 'nomor_telepon' => '081211122233', 'status' => 'nonaktif'],
     ];
 
     public function index()
